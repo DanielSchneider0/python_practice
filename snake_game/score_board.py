30016fa5f00e416800d93b9d364db806f6a5f869
Daniel Schneider <daniel.schneider00@gmail.com>
2022-06-21 13:27:03 -0500

    final snake product

---
+++ snake_game/score_board.py
@@ -0,0 +1,30 @@
+from turtle import Turtle
+
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(
+            f"Score: {self.score}", align="center", font=("Verdana", 15, "normal")
+        )
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(
+            f"GAMEOVER. Score: {self.score}",
+            align="center",
+            font=("Verdana", 15, "normal"),
+        )
+
+    def increase(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
